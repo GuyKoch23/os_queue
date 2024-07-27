782d4bdfe82e97eaf5a06efd46ed485662abb2b3
GuyKoch <guykoch@mail.tau.ac.il>
2024-07-27 12:30:31 +0300

    finished hw

--- queue.c
+++ queue.c
@@ -3,6 +3,8 @@
 #include <threads.h>
 #include "queue.h"
 
+// items are represented by the ItemNode
+// linked list of items waiting in the queue
 typedef struct ItemNode {
     void* data;
     int id;
@@ -10,12 +12,17 @@ typedef struct ItemNode {
     int isPaired;
 } ItemNode;
 
+// Threads are represented by its condition varaible
+// linked list of threads waiting to dequeue items from the queue
 typedef struct CondVarsNode {
     cnd_t cnd;
     int id;
     struct CondVarsNode* next;
 } CondVarsNode;
 
+// The queue structure will hold two linked lists
+// ItemNode list for the items in the queue
+// CondVarsNode list for the threads waiting to dequeue
 typedef struct Queue{
     ItemNode *item_head;
     ItemNode *item_last;
@@ -23,15 +30,14 @@ typedef struct Queue{
     CondVarsNode *cv_last;
     mtx_t mtx;
     size_t waiting;
-    size_t paired;
+    size_t paired; // number of currently paired items in queue to waiting thread which still havent dequeue the item
     size_t visited;
     size_t size;
-    size_t count;
+    size_t count; // kind of id counter for the items inserting to the queue
 } Queue;
 
-
+// The Queue of the app
 Queue queue;
-
 
 void initQueue(void){
     queue.waiting = 0;
@@ -49,6 +55,7 @@ void initQueue(void){
 void destroyQueue(void){
     ItemNode *node = queue.item_head;
     ItemNode *tmp;
+    //free memory for items structs allocated
     while(node != NULL){
         tmp = node;
         node = node->next;
@@ -56,6 +63,7 @@ void destroyQueue(void){
     }
     CondVarsNode *node2 = queue.cv_head;
     CondVarsNode *tmp2;
+    //free memory for condition vars structs allocated
     while(node2 != NULL){
         tmp2 = node2;
         node2 = node2->next;
@@ -87,10 +95,10 @@ void enqueue(void* data){
     newNode->data = data;
     newNode->id = queue.count++;
     newNode->next = NULL;
-    newNode->isPaired = 0;
+    newNode->isPaired = 0; // indicates if the item already paired to some thread and considered "not available for dequeue"
 
     // iserting new data to queue
-    if(queue.size == 0){
+    if(queue.size == 0){ // only item in the queue
         queue.item_head = newNode;
         queue.item_last = newNode;
     }
@@ -103,14 +111,14 @@ void enqueue(void* data){
     // pair data with waiting thread and wake it up
     if(queue.waiting > queue.paired){
         CondVarsNode *node = queue.cv_head;
-        while(node != NULL && node->id != -1){
+        while(node != NULL && node->id != -1){ // find first thread can be paired with data
             node = node->next;
         }
         if(node != NULL){
             node->id = newNode->id; // pair the thread with the item
             newNode->isPaired = 1;
             queue.paired++;
-            cnd_signal(&node->cnd);
+            cnd_signal(&node->cnd); // wake up the thread since it can dequeue the data
         }
     }
     mtx_unlock(&queue.mtx);
@@ -123,18 +131,18 @@ void* dequeue(void){
         fprintf(stderr, "Allocation problem");
         exit(EXIT_FAILURE);
     }
-    newNode->id = -1;
+    newNode->id = -1; // sign for un-paried thread
 
-    if(queue.size <= queue.paired){
+    if(queue.size <= queue.paired){ // no data available - might empty queue or all paired
         cnd_init(&newNode->cnd);
         newNode->next = NULL;
 
         // insert thread to waiting thread list
-        if(queue.cv_head == NULL){
+        if(queue.cv_head == NULL){ // only thread
             queue.cv_head = newNode;
             queue.cv_last = newNode;
         }
-        else{
+        else{ // insert as last
             queue.cv_last->next = newNode;
             queue.cv_last = newNode;
         }
@@ -148,13 +156,13 @@ void* dequeue(void){
     ItemNode *node = queue.item_head;
     ItemNode *prev = NULL;
     void* data = NULL;
-    if(newNode->id != -1){ // sleeping-paired thread which woke up
+    if(newNode->id != -1){ // the woke up thread is paired, need to find paired item
         while(node != NULL && node->id != newNode->id){
             prev = node;
             node = node->next;
         }
         data = node->data;
-        if(prev == NULL){
+        if(prev == NULL){ // remove item from list
             queue.item_head = queue.item_head->next;
             if(queue.item_head == NULL){
                 queue.item_last = NULL;
@@ -162,17 +170,17 @@ void* dequeue(void){
         }
         else{
             prev->next = node->next;
-            if(node->next == NULL){ // item_last
+            if(node->next == NULL){
                 queue.item_last = prev;
             }
         }
     }
-    else{
-        while(node != NULL && node->isPaired == 1){
+    else{ // thread just arrived, not paired, but item is availble 
+        while(node != NULL && node->isPaired == 1){ // skip all paired items
             prev = node;
             node = node->next;
         }
-        data = node->data;
+        data = node->data; // first un-paired data
         if(prev == NULL){
             queue.item_head = queue.item_head->next;
             if(queue.item_head == NULL){
@@ -181,7 +189,7 @@ void* dequeue(void){
         }
         else{
             prev->next = node->next;
-            if(node->next == NULL){ // item_last
+            if(node->next == NULL){
                 queue.item_last = prev;
             }
         }
@@ -191,7 +199,7 @@ void* dequeue(void){
         queue.paired--;
     }
     queue.visited++;
-    if(newNode->id != -1){
+    if(newNode->id != -1){ // remove thread from waiting list
         CondVarsNode *cvnode = queue.cv_head;
         CondVarsNode *cvnode_prev = NULL;
         while(cvnode != NULL && cvnode->id != newNode->id){
@@ -219,7 +227,7 @@ void* dequeue(void){
 
 bool tryDequeue(void** data){
     mtx_lock(&queue.mtx);
-    if(queue.size <= queue.paired){
+    if(queue.size <= queue.paired){ // no items available
         mtx_unlock(&queue.mtx);
         return false;
     }
@@ -227,7 +235,7 @@ bool tryDequeue(void** data){
     // take appropriate data according to id
     ItemNode *node = queue.item_head;
     ItemNode *prev = NULL;
-    while(node->isPaired == 1 || node == NULL){
+    while(node->isPaired == 1 || node == NULL){ // find first available item
         prev = node;
         node = node->next;
     }
@@ -236,15 +244,15 @@ bool tryDequeue(void** data){
         return false;
     }
     *data = node->data;
-    if(prev == NULL){
+    if(prev == NULL){ // remove item from list, first item
         queue.item_head = queue.item_head->next;
         if(queue.item_head == NULL){
             queue.item_last = NULL;
         }
     }
-    else{
+    else{// remove item from list, non-first item
         prev->next = node->next;
-        if(node->next == NULL){ // item_last
+        if(node->next == NULL){
             queue.item_last = prev;
         }
     }
@@ -254,7 +262,6 @@ bool tryDequeue(void** data){
     mtx_unlock(&queue.mtx);
     return true;
 }
-
 
 size_t size(void){
     return queue.size;
